da0f73f264cc3f8dd3cd27374d88968443706044
bryceAM <bryce.malmberg@yahoo.com>
2023-10-03 23:40:01 -0700

    feat: #2 src folder architecture

---
+++ src/components/Header.ts
@@ -0,0 +1,10 @@
+/*
+import react
+
+export default function Header() {
+    return (
+        <>
+        </>
+    )
+}
+*/
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+export default {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: 'src/assets/styles', 
+                to: 'assets/styles'
+            }]
+        })
+    ]
+};
