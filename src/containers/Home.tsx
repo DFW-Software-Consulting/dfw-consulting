837d1d70afae76166e9e62b8c427c6af6b10e901
bryceAM <bryce.malmberg@yahoo.com>
2023-11-28 21:33:14 -0800

    feat: #37 add Home.tsx container filler

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Home = () => {
     return (
         <>
+            <div>Home!</div>
+            <br />
+            <div>more home!</div>
+            <br />
+            <div>feeling at home yet?</div>
         </>
     )
 }
