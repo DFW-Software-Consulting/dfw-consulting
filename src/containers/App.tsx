6c8cb18c3a1f63bcd456aa5ba3c0ab9f94703381
bryceAM <bryce.malmberg@yahoo.com>
2023-12-03 21:41:42 -0800

    feat: #43 add header and footer wrappers and team component to App

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,35 +3,43 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { default as Home } from './Home';
 import { default as About } from './About';
-import { default as Contact } from './Contact';
+import { default as Team } from './Team';
 import { Header, Footer } from '../components';
 
 // components
 const App = () => {
    
     return (
-        <div id="wrapper">
-            <Header />
-            <Routes>
-                <Route
-                    path="/about"
-                    element={<About />}
-                />
-                <Route
-                    path="/contact"
-                    element={<Contact />}
-                />
-                <Route
-                    path="/home"
-                    element={<Home />}
-                />
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
-            </Routes>
-            <Footer />
+        <>
+         <div id="wrapper">
+            <div id="header-wrapper">
+                <Header />
+            </div>
+            <div id="main">
+                <Routes>
+                    <Route
+                        path="/about"
+                        element={<About />}
+                    />
+                    <Route
+                        path="/team"
+                        element={<Team />}
+                    />
+                    <Route
+                        path="/home"
+                        element={<Home />}
+                    />
+                    <Route
+                        path="/"
+                        element={<Home />}
+                    />
+                </Routes>
+            </div>
+            <div id="footer-wrapper">
+                <Footer />
+            </div>
         </div>
+        </>
     )
 }
 
