6c796145259a479478ea676ab712d7b93bec6ed2
bryceAM <bryce.malmberg@yahoo.com>
2023-12-17 23:16:14 -0800

    feat: #59 index.ts for images folder

---
+++ src/assets/images/index.ts
@@ -0,0 +1,16 @@
+import stafferOne from './staffer1.png';
+import stafferTwo from './staffer2.png';
+import stafferThree from './staffer3.png';
+import stafferFour from './staffer4.png';
+import stafferFive from './staffer5.png';
+import stafferSix from './staffer6.png';
+
+
+export {
+    stafferOne,
+    stafferTwo,
+    stafferThree,
+    stafferFour,
+    stafferFive,
+    stafferSix
+};
