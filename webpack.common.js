1c60b78bf33242bd986b33653547ec9cbf49f5f1
bryceAM <bryce.malmberg@yahoo.com>
2023-12-17 23:05:55 -0800

    feat: #57 add image support to webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,16 @@ const config = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'assets/images/[name].[ext]'
+                    }
+                }
             }
         ]
     },
