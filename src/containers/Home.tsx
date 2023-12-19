f0580ccc4663b680600e30128625b80b439ebfc0
bryceAM <bryce.malmberg@yahoo.com>
2023-12-18 18:51:27 -0800

    feat: #67 add minimum home page content and styles

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import '../assets/styles/home.css';
 
 const Home = () => {
     return (
         <>
-            <div>Home!</div>
-            <br />
-            <div>more home!</div>
-            <br />
-            <div>feeling at home yet?</div>
+            <div className="page-title">
+                <div className="page-title-inner">dfw.home</div>
+            </div>
+            <div className="left-card-small">leftCardSmall</div>
+            <div className="right-card-big">rightCardBig</div>
+            <div className="left-card-big">leftCardBig</div>
+            <div className="right-card-small">rightCardSmall</div>
+            <div className="content-card">contentCard</div>
         </>
     )
 }
 
 export default Home;
+
+/*
+    play around with the grid, adding free stock photos
+    with a positive inspiring mission statement and question
+    incentivizing companies to take a further look into
+    the website
+
+    - get stock photos that match the theme of text points
+    - generate text for the text points that's positive and
+    inspiring
+    - stagger photos. placer 1 in column 1 row 1, the next
+    photo in column 3, row 2, the next one in column 1, row 3,
+    etc. and have the text take up 2 columns and 1 row each
+    and be opposite of the themed stock photo they're describing 
+*/
