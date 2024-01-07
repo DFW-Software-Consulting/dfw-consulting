ad59a428137d92b5baa891ecf170e4828edac34f
bryceAM <bryce.malmberg@yahoo.com>
2024-01-07 01:03:07 -0800

    feat: #78 App container hero image

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,6 +1,6 @@
 // imports
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { default as Home } from './Home';
 import { default as About } from './About';
 import { default as Team } from './Team';
@@ -8,37 +8,44 @@ import { Header, Footer } from '../components';
 
 // components
 const App = () => {
+    const location = useLocation();
    
     return (
         <>
-         <div id="wrapper">
-            <div id="header-wrapper">
-                <Header />
+            <div id="wrapper">
+                <div id="header-wrapper">
+                    <Header />
+                </div>
+                {(location.pathname === '/Home' || location.pathname === '/') &&
+                    <div className="hero">
+                        <div className="hero-title">DFW Consulting</div>
+                        <div className="hero-quote">Within every new idea is the power to change the world</div>
+                    </div>
+                }
+                <div id="main">
+                    <Routes>
+                        <Route
+                            path="/about"
+                            element={<About />}
+                        />
+                        <Route
+                            path="/team"
+                            element={<Team />}
+                        />
+                        <Route
+                            path="/home"
+                            element={<Home />}
+                        />
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        />
+                    </Routes>
+                </div>
+                <div id="footer-wrapper">
+                    <Footer />
+                </div>
             </div>
-            <div id="main">
-                <Routes>
-                    <Route
-                        path="/about"
-                        element={<About />}
-                    />
-                    <Route
-                        path="/team"
-                        element={<Team />}
-                    />
-                    <Route
-                        path="/home"
-                        element={<Home />}
-                    />
-                    <Route
-                        path="/"
-                        element={<Home />}
-                    />
-                </Routes>
-            </div>
-            <div id="footer-wrapper">
-                <Footer />
-            </div>
-        </div>
         </>
     )
 }
